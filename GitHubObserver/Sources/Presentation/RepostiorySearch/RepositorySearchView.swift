09bc2bc926dadb6a6e614b3d1bc901bf9ae6eaf1
seung uk <69791286+shinseunguk@users.noreply.github.com>
2024-09-11 16:15:34 +0900

    ⚡️ [Feat] 다국어 처리 및 하단 탭바 구현

---
+++ GitHubObserver/Sources/Presentation/RepostiorySearch/RepositorySearchView.swift
@@ -0,0 +1,21 @@
+//
+//  RepositorySearchView.swift
+//  GitHubObserver
+//
+//  Created by ukseung.dev on 9/11/24.
+//
+
+import SwiftUI
+
+public struct RepositorySearchView: View {
+    
+    public var body: some View {
+        VStack {
+            Text("RepositorySearchView")
+        }
+    }
+}
+
+#Preview {
+    RepositorySearchView()
+}
